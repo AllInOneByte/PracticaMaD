b1939c6cfa38f02ddc29e71ed849032a9f515b13
Alejandro Pereira Carballo <alejandro.pereira.carballo@udc.es>
2020-11-15 20:23:23 +0100

    Creados ficheros de DAO de Labeled

---
+++ Model/LabeledDao/ILabeledDao.cs
@@ -0,0 +1,8 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.LabeledDao
+{
+    public interface ILabeledDao : IGenericDao<Labeled, long>
+    {
+    }
+}
---
+++ Model/LabeledDao/LabeledDaoEntityFramework.cs
@@ -0,0 +1,22 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.LabeledDao
+{
+    /// <summary>
+    /// Specific Operations for Labeled
+    /// </summary>
+    public class LabeledDaoEntityFramework :
+        GenericDaoEntityFramework<Labeled, long>, ILabeledDao
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        public LabeledDaoEntityFramework()
+        {
+        }
+
+        #endregion Public Constructors
+    }
+}
