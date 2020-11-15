5be557fe04693f81138b4b82020b77865770ef25
Alejandro Pereira Carballo <alejandro.pereira.carballo@udc.es>
2020-11-15 20:19:22 +0100

    Creados ficheros de DAO de DeliveryLine

---
+++ Model/DeliveryLineDao/DeliveryDaoEntityFramework.cs
@@ -0,0 +1,22 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.DeliveryLineDao
+{
+    /// <summary>
+    /// Specific Operations for Delivery
+    /// </summary>
+    public class DeliveryLineDaoEntityFramework :
+        GenericDaoEntityFramework<DeliveryLine, long>, IDeliveryLineDao
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        public DeliveryLineDaoEntityFramework()
+        {
+        }
+
+        #endregion Public Constructors
+    }
+}
---
+++ Model/DeliveryLineDao/IDeliveryDao.cs
@@ -0,0 +1,8 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.DeliveryLineDao
+{
+    public interface IDeliveryLineDao : IGenericDao<DeliveryLine, long>
+    {
+    }
+}
