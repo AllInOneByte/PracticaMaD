f61ab623f46fe715612715e8956c7eddbb30b7e8
Gs99-doc <73192489+Gs99-doc@users.noreply.github.com>
2020-11-15 20:35:59 +0100

    Dao para Tag

---
+++ Model/TagDao/ITagDao.cs
@@ -0,0 +1,8 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.TagDao
+{
+    public interface ITagDao : IGenericDao<Tag, long>
+    {
+    }
+}
---
+++ Model/TagDao/TagDaoEntityFramework.cs
@@ -0,0 +1,19 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.TagDao
+{
+    class TagDaoEntityFramework :
+         GenericDaoEntityFramework<Tag, long>, ITagDao
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        public TagDaoEntityFramework()
+        {
+        }
+
+        #endregion Public Constructors
+    }
+}
