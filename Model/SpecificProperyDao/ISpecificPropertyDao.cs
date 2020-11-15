afa5fd7cd6a323541742ac78016a724f457908c6
Alejandro Pereira Carballo <alejandro.pereira.carballo@udc.es>
2020-11-15 20:27:13 +0100

    Creados ficheros de DAO de SpecificProperty

---
+++ Model/SpecificProperyDao/ISpecificPropertyDao.cs
@@ -0,0 +1,8 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.SpecificProperty
+{
+    public interface ISpecificPropertyDao : IGenericDao<SpecificProperty, long>
+    {
+    }
+}
---
+++ Model/SpecificProperyDao/SpecificPropertyDaoEntityFramework.cs
@@ -0,0 +1,22 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.SpecificProperty
+{
+    /// <summary>
+    /// Specific Operations for SpecificProperty
+    /// </summary>
+    public class SpecificPropertyDaoEntityFramework :
+        GenericDaoEntityFramework<SpecificProperty, long>, ISpecificPropertyDao
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        public SpecificPropertyDaoEntityFramework()
+        {
+        }
+
+        #endregion Public Constructors
+    }
+}
