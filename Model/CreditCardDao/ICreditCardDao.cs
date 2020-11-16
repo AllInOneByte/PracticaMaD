861803bb7f2a513ed98e565bae12b953f82952dd
Ramiro Serantes Garrido <ramiro.serantes@udc.es>
2020-11-16 18:37:44 +0100

    Añadiendo la configuración del CreditCardDao

--- Model/CreditCardDao/ICreditCardDao.cs
+++ Model/CreditCardDao/ICreditCardDao.cs
@@ -1,8 +1,28 @@
-﻿using Es.Udc.DotNet.ModelUtil.Dao;
+﻿using System.Collections.Generic;
+using Es.Udc.DotNet.ModelUtil.Dao;
 
 namespace Es.Udc.DotNet.PracticaMaD.Model.CreditCardDao
 {
     public interface ICreditCardDao : IGenericDao<CreditCard, long>
     {
+   
+        /// <summary>
+        /// Finds a CreditCard by userId
+        /// </summary>
+        /// <param name="userId">userId</param>
+        /// <returns>List of CreditCard</returns>
+        /// <exception cref="InstanceNotFoundException"/>
+        List<CreditCard> FindByUserId(long userId);
+
+        /// <summary>
+        /// Finds a default CreditCard by userId 
+        /// </summary>
+        /// <param name="userId">userId</param>
+        /// <returns>The Default CreditCard</returns>
+        /// <exception cref="InstanceNotFoundException"/>
+        CreditCard FindDefaultUserIdCard(long userId);
+
+
     }
 }
+
