e615743f62783e9770ed2d04abb485a369d13e74
Ramiro Serantes Garrido <ramiro.serantes@udc.es>
2020-11-16 21:22:25 +0100

    Implementado DeliveryDaoEntityFramework, FindByUserId

--- Model/DeliveryDao/DeliveryDaoEntityFramework.cs
+++ Model/DeliveryDao/DeliveryDaoEntityFramework.cs
@@ -1,4 +1,8 @@
-﻿using Es.Udc.DotNet.ModelUtil.Dao;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Es.Udc.DotNet.ModelUtil.Exceptions;
+using Es.Udc.DotNet.ModelUtil.Dao;
 
 namespace Es.Udc.DotNet.PracticaMaD.Model.DeliveryDao
 {
@@ -9,7 +13,6 @@ namespace Es.Udc.DotNet.PracticaMaD.Model.DeliveryDao
         GenericDaoEntityFramework<Delivery, long>, IDeliveryDao
     {
         #region Public Constructors
-
         /// <summary>
         /// Public Constructor
         /// </summary>
@@ -18,5 +21,38 @@ namespace Es.Udc.DotNet.PracticaMaD.Model.DeliveryDao
         }
 
         #endregion Public Constructors
+
+        #region IDeliveryDao Members. Specific Operations
+        /// <summary>
+        /// Finds a List of Deliverys by userId
+        /// </summary>
+        /// <param userId="userId">userId</param>
+        /// <returns>List of Deliverys</returns>
+        /// <exception cref="InstanceNotFoundException"/>
+        List<Delivery> IDeliveryDao.FindByUserId(long userId)
+        {
+            List<Delivery> delivery = null;
+
+            #region Option 1: Using Linq.
+
+            DbSet<Delivery> deliveryFound = Context.Set<Delivery>();
+
+            var result =
+                (from d in deliveryFound
+                 where d.userId == userId
+                 select d);
+
+            delivery = result.ToList();
+
+            #endregion Option 1: Using Linq.
+
+            if (deliveryFound == null)
+                throw new InstanceNotFoundException(userId,
+                    typeof(DeliveryLine).FullName);
+
+            return delivery;
+        }
+
+        #endregion IDeliveryLineDao Members. Specific Operations
     }
 }
