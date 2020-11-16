b12f389635ffcb0c5c38d962865a86966f7fb80d
Alejandro Pereira Carballo <alejandro.pereira.carballo@udc.es>
2020-11-16 19:51:59 +0100

    Añadido método a la interfaz de ICommentDao

---
+++ Model/CommentDao/CommentDaoEntityFramework.cs
@@ -0,0 +1,22 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.CommentDao
+{
+    /// <summary>
+    /// Specific Operations for Comment
+    /// </summary>
+    public class CommentDaoEntityFramework :
+        GenericDaoEntityFramework<Comment, long>, ICommentDao
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Public Constructor
+        /// </summary>
+        public CommentDaoEntityFramework()
+        {
+        }
+
+        #endregion Public Constructors
+    }
+}
---
+++ Model/CommentDao/ICommentDao.cs
@@ -0,0 +1,16 @@
+﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using System.Collections.Generic;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.CommentDao
+{
+    public interface ICommentDao : IGenericDao<Comment, long>
+    {
+        /// <summary>
+        /// Finds a List of Comments by productId
+        /// </summary>
+        /// <param name="productId">productId</param>
+        /// <returns>List of Comment</returns>
+        /// <exception cref="InstanceNotFoundException"/>
+        List<Comment> FindByProductIdOrderByDeliveryDate(long productId);
+    }
+}
