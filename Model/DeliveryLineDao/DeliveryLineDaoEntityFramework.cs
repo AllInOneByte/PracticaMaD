d67f5e813677d452d5a301ffcef380b49d1727cd
Alejandro Pereira Carballo <alejandro.pereira.carballo@udc.es>
2020-11-16 20:34:27 +0100

    Implementada función de FindByDeliveryId en DAO de DeliveryLine

--- Model/DeliveryLineDao/DeliveryLineDaoEntityFramework.cs
+++ Model/DeliveryLineDao/DeliveryLineDaoEntityFramework.cs
@@ -1,4 +1,8 @@
 ﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using Es.Udc.DotNet.ModelUtil.Exceptions;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 
 namespace Es.Udc.DotNet.PracticaMaD.Model.DeliveryLineDao
 {
@@ -18,5 +22,33 @@ namespace Es.Udc.DotNet.PracticaMaD.Model.DeliveryLineDao
         }
 
         #endregion Public Constructors
+
+        #region IDeliveryLineDao Members. Specific Operations
+
+        public List<DeliveryLine> FindByDeliveryId(long deliveryId)
+        {
+            List<DeliveryLine> deliveryLines = null;
+
+            #region Option 1: Using Linq.
+
+            DbSet<DeliveryLine> deliveryLinesFound = Context.Set<DeliveryLine>();
+
+            var result =
+                (from d in deliveryLinesFound
+                 where d.deliveryId == deliveryId
+                 select d);
+
+            deliveryLines = result.ToList();
+
+            #endregion Option 1: Using Linq.
+
+            if (deliveryLines == null)
+                throw new InstanceNotFoundException(deliveryId,
+                    typeof(DeliveryLine).FullName);
+
+            return deliveryLines;
+        }
+
+        #endregion IDeliveryLineDao Members. Specific Operations
     }
 }
