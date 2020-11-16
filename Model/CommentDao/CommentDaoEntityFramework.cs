9ab95badca5ece7fb40c78e3ca07a2d73aceab80
Alejandro Pereira Carballo <alejandro.pereira.carballo@udc.es>
2020-11-16 20:26:43 +0100

    Implementada función de FindByProductIdOrderByDeliveryDate en DAO de Comment

--- Model/CommentDao/CommentDaoEntityFramework.cs
+++ Model/CommentDao/CommentDaoEntityFramework.cs
@@ -1,4 +1,8 @@
 ﻿using Es.Udc.DotNet.ModelUtil.Dao;
+using Es.Udc.DotNet.ModelUtil.Exceptions;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
 
 namespace Es.Udc.DotNet.PracticaMaD.Model.CommentDao
 {
@@ -18,5 +22,34 @@ namespace Es.Udc.DotNet.PracticaMaD.Model.CommentDao
         }
 
         #endregion Public Constructors
+
+        #region ICommentDao Members. Specific Operations
+
+        public List<Comment> FindByProductIdOrderByDeliveryDate(long productId)
+        {
+            List<Comment> comments = null;
+
+            #region Option 1: Using Linq.
+
+            DbSet<Comment> commentsFound = Context.Set<Comment>();
+
+            var result =
+                (from c in commentsFound
+                 where c.productId == productId
+                 orderby c.commentDate descending
+                 select c);
+
+            comments = result.ToList();
+
+            #endregion Option 1: Using Linq.
+
+            if (comments == null)
+                throw new InstanceNotFoundException(productId,
+                    typeof(Comment).FullName);
+
+            return comments;
+        }
+
+        #endregion ICommentDao Members. Specific Operations
     }
 }
