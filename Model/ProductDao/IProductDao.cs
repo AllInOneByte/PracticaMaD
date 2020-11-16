6de2f218d63eef8a483892752a21ac702ceb5a97
Ramiro Serantes Garrido <ramiro.serantes@udc.es>
2020-11-16 18:58:51 +0100

    Añadiendo la configuración del ProductDao

--- Model/ProductDao/IProductDao.cs
+++ Model/ProductDao/IProductDao.cs
@@ -1,8 +1,17 @@
+using System.Collections.Generic;
 using Es.Udc.DotNet.ModelUtil.Dao;
 
 namespace Es.Udc.DotNet.PracticaMaD.Model.ProductDao
 {
     public interface IProductDao : IGenericDao<Product, long>
     {
+        /// <summary>
+        /// Finds a Product by Key words or Category
+        /// </summary>
+        /// <param keyWords="keyWords">keyWords</param>
+        /// <param categoryId="categoryId">categoryId</param>
+        /// <returns>List of Product</returns>
+        /// <exception cref="InstanceNotFoundException"/>
+        List<Product> FindByKeywordsAndCategory(string keyWords, long categoryId);
     }
 }
