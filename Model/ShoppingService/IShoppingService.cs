bcbf77e153eff4c58e8a33ae926293ae63a1749b
Alejandro Pereira Carballo <alejandro.pereira.carballo@udc.es>
2020-11-18 19:46:04 +0100

    Hecho servicio de compras

---
+++ Model/ShoppingService/IShoppingService.cs
@@ -0,0 +1,50 @@
+﻿using Es.Udc.DotNet.ModelUtil.Exceptions;
+using Es.Udc.DotNet.ModelUtil.Transactions;
+using Es.Udc.DotNet.PracticaMaD.Model.CreditCardDao;
+using Es.Udc.DotNet.PracticaMaD.Model.DeliveryDao;
+using Es.Udc.DotNet.PracticaMaD.Model.DeliveryLineDao;
+using Es.Udc.DotNet.PracticaMaD.Model.UserProfileDao;
+using Ninject;
+using System.Collections.Generic;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.ShoppingService
+{
+    public interface IShoppingService
+    {
+        [Inject]
+        IUserProfileDao UserProfileDao { set; }
+        ICreditCardDao CreditCardDao { set; }
+        IDeliveryDao DeliveryDao { set; }
+        IDeliveryLineDao DeliveryLineDao { set; }
+
+        /// <summary>
+        /// Creates a new delivery object from the shopping cart.
+        /// </summary>
+        /// <param name="shoppingCart"> The shopping cart data. </param>
+        /// <param name="userId"> The user profile id. </param>
+        /// <param name="cardId"> The credit card id. </param>
+        /// <returns> The created delivery </returns>
+        /// <exception cref="InstanceNotFoundException"/>
+        [Transactional]
+        ShoppingCart CreateDelivery(ShoppingCart shoppingCart,
+            List<ShoppingCartDetails> shoppingCartDetails);
+
+        /// <summary>
+        /// Retrieves all the deliveries of the user.
+        /// </summary>
+        /// <param name="userId"> The user profile id. </param>
+        /// <returns> A list with all the deliveries from the user </returns>
+        /// <exception cref="InstanceNotFoundException"/>
+        [Transactional]
+        List<ShoppingCart> GetAllDeliveries(long userId);
+
+        /// <summary>
+        /// Gets the details of a specific delivery.
+        /// </summary>
+        /// <param name="deliveryId"> The delivery id. </param>
+        /// <returns> The details of the delivery </returns>
+        /// <exception cref="InstanceNotFoundException"/>
+        [Transactional]
+        List<ShoppingCartDetails> GetDeliveryDetails(long deliveryId);
+    }
+}
---
+++ Model/ShoppingService/ShoppingService.cs
@@ -0,0 +1,103 @@
+﻿using Es.Udc.DotNet.ModelUtil.Exceptions;
+using Es.Udc.DotNet.ModelUtil.Transactions;
+using Es.Udc.DotNet.PracticaMaD.Model.CreditCardDao;
+using Es.Udc.DotNet.PracticaMaD.Model.DeliveryDao;
+using Es.Udc.DotNet.PracticaMaD.Model.DeliveryLineDao;
+using Es.Udc.DotNet.PracticaMaD.Model.UserProfileDao;
+using System;
+using System.Collections.Generic;
+
+namespace Es.Udc.DotNet.PracticaMaD.Model.ShoppingService
+{
+    public class ShoppingService : IShoppingService
+    {
+        public IUserProfileDao UserProfileDao { private get; set; }
+        public ICreditCardDao CreditCardDao { private get; set; }
+        public IDeliveryDao DeliveryDao { private get; set; }
+        public IDeliveryLineDao DeliveryLineDao { private get; set; }
+
+        #region IShoppingService members
+
+        /// <exception cref="InstanceNotFoundException"/>
+        [Transactional]
+        public ShoppingCart CreateDelivery(ShoppingCart shoppingCart,
+            List<ShoppingCartDetails> shoppingCartDetails)
+        {
+            if (CreditCardDao.FindByUserId(shoppingCart.UserId)
+                .Contains(CreditCardDao.Find(shoppingCart.CardId)))
+            {
+                Delivery delivery = new Delivery
+                {
+                    cardId = shoppingCart.CardId,
+                    userId = shoppingCart.UserId,
+                    deliveryAddress = shoppingCart.DeliveryAddress,
+                    deliveyDate = shoppingCart.DeliveryDate,
+                    deliveryPrice = shoppingCart.DeliveryPrice,
+                    description = shoppingCart.Description
+                };
+
+                DeliveryDao.Create(delivery);
+
+                foreach (ShoppingCartDetails item in shoppingCartDetails)
+                {
+                    DeliveryLine line = new DeliveryLine
+                    {
+                        deliveryLineAmount = item.DeliveryLineAmount,
+                        deliveryLinePrice = item.DeliveryLinePrice,
+                        productId = item.ProductId,
+                        deliveryId = delivery.deliveryId
+                    };
+
+                    DeliveryLineDao.Create(line);
+                }
+
+                shoppingCart.DeliveryId = delivery.deliveryId;
+
+                return shoppingCart;
+                
+            }
+            else
+            {
+                throw new Exception(); /// TODO: make a custom exception like UnmatchingUserAndCardException 
+            }
+        }
+
+        /// <exception cref="InstanceNotFoundException"/>
+        [Transactional]
+        public List<ShoppingCart> GetAllDeliveries(long userId)
+        {
+            List<Delivery> deliveries = DeliveryDao.FindByUserId(userId);
+            List<ShoppingCart> shoppingCarts = new List<ShoppingCart>();
+
+            foreach (Delivery delivery in deliveries)
+            {
+                ShoppingCart shoppingCart = new ShoppingCart(delivery.deliveyDate,
+                    delivery.deliveryPrice, delivery.deliveryAddress,
+                    delivery.description, delivery.userId, delivery.cardId);
+
+                shoppingCart.DeliveryId = delivery.deliveryId;
+
+                shoppingCarts.Add(shoppingCart);
+            }
+
+            return shoppingCarts;
+        }
+
+        /// <exception cref="InstanceNotFoundException"/>
+        [Transactional]
+        public List<ShoppingCartDetails> GetDeliveryDetails(long deliveryId)
+        {
+            List<DeliveryLine> lines = DeliveryLineDao.FindByDeliveryId(deliveryId);
+            List<ShoppingCartDetails> details = new List<ShoppingCartDetails>();
+
+            foreach (DeliveryLine line in lines)
+            {
+                details.Add(new ShoppingCartDetails(line.deliveryLineAmount, line.deliveryLinePrice, line.deliveryId, line.productId));
+            }
+
+            return details;
+        }
+
+        #endregion IShoppingService members
+    }
+}
