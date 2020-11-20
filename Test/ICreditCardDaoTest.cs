16e480409819babefc152dd73953eb34c7bf0757
Alejandro Pereira Carballo <alejandro.pereira.carballo@udc.es>
2020-11-20 21:39:46 +0100

    Añadido esqueleto para tests de CreditCardDao

--- Test/ICreditCardDaoTest.cs
+++ Test/ICreditCardDaoTest.cs
@@ -2,6 +2,8 @@
 using Es.Udc.DotNet.PracticaMaD.Model.CreditCardDao;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Ninject;
+using System;
+using System.Collections.Generic;
 using System.Transactions;
 
 namespace Es.Udc.DotNet.PracticaMaD.Test
@@ -11,18 +13,87 @@ namespace Es.Udc.DotNet.PracticaMaD.Test
     {
         private static IKernel kernel;
         private static ICreditCardDao creditCardDao;
-        private static CreditCard creditCard;
+        private static List<CreditCard> creditCards;
 
         // Variables used in several tests are initialized here
-        private const long NON_EXISTENT_USER_ID = -1;
+        private const long CARD_NUMBER = 5231962446920945;
 
         private TransactionScope transaction;
 
-        private TestContext testContextInstance;
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext { get; set; }
 
-        [TestMethod]
-        public void TestMethod1()
+        #region Additional test attributes
+
+        //Use ClassInitialize to run code before running the first test in the class
+        [ClassInitialize()]
+        public static void MyClassInitialize(TestContext testContext)
         {
+            kernel = TestManager.ConfigureNInjectKernel();
+            creditCardDao = kernel.Get<ICreditCardDao>();
+        }
+
+        //Use ClassCleanup to run code after all tests in a class have run
+        [ClassCleanup()]
+        public static void MyClassCleanup()
+        {
+            TestManager.ClearNInjectKernel(kernel);
+        }
+
+        //Use TestInitialize to run code before running each test
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            transaction = new TransactionScope();
+
+            CreditCard defaultCreditCard = new CreditCard
+            {
+                cardNumber = CARD_NUMBER, // hay que hacer que acepte long en vez de int
+                cardType = "plastic????", // no idea what this is
+                expirationDate = DateTime.Now.AddMonths(1),
+                userId = 1,
+                verificationCode = 1 // no idea what is this either, gotta ask
+            };
+            // pendiente de guardar en bd y crear el resto de tarjetas
+            creditCards.Add(defaultCreditCard);
+        }
+
+        //Use TestCleanup to run code after each test has run
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            transaction.Dispose();
+        }
+
+        #endregion Additional test attributes
+
+        [TestMethod()]
+        public void DAO_FindByUserId()
+        {
+            try
+            {
+                //TODO
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
+        }
+
+        [TestMethod()]
+        public void DAO_FindDefaultUserIdCard()
+        {
+            try
+            {
+                //TODO
+            }
+            catch (Exception e)
+            {
+                Assert.Fail(e.Message);
+            }
         }
     }
 }
---
+++ Test/TestManager.cs
@@ -0,0 +1,58 @@
+﻿using Es.Udc.DotNet.PracticaMaD.Model.CreditCardDao;
+using Es.Udc.DotNet.PracticaMaD.Model.ShoppingService;
+using Ninject;
+using System.Configuration;
+using System.Data.Entity;
+
+namespace Es.Udc.DotNet.PracticaMaD.Test
+{
+    class TestManager
+    {
+        /// <summary>
+        /// Configures and populates the Ninject kernel
+        /// </summary>
+        /// <returns>The NInject kernel</returns>
+        public static IKernel ConfigureNInjectKernel()
+        {
+            NinjectSettings settings = new NinjectSettings() { LoadExtensions = true };
+
+            IKernel kernel = new StandardKernel(settings);
+
+            kernel.Bind<ICreditCardDao>().
+                To<CreditCardDaoEntityFramework>();
+
+            kernel.Bind<IShoppingService>().
+                To<ShoppingService>();
+
+            string connectionString =
+                ConfigurationManager.ConnectionStrings["MiniPortalEntities"].ConnectionString;
+
+            kernel.Bind<DbContext>().
+                ToSelf().
+                InSingletonScope().
+                WithConstructorArgument("nameOrConnectionString", connectionString);
+
+            return kernel;
+        }
+
+        /// <summary>
+        /// Configures the Ninject kernel from an external module file.
+        /// </summary>
+        /// <param name="moduleFilename">The module filename.</param>
+        /// <returns>The NInject kernel</returns>
+        public static IKernel ConfigureNInjectKernel(string moduleFilename)
+        {
+            NinjectSettings settings = new NinjectSettings() { LoadExtensions = true };
+            IKernel kernel = new StandardKernel(settings);
+
+            kernel.Load(moduleFilename);
+
+            return kernel;
+        }
+
+        public static void ClearNInjectKernel(IKernel kernel)
+        {
+            kernel.Dispose();
+        }
+    }
+}
