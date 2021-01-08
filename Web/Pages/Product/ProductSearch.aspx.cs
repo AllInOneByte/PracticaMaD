d8c937b4df13ee7d66c5fe90a9c8b64a49a340ad
Alejandro Pereira Carballo <alejandro.pereira.carballo@udc.es>
2021-01-08 20:24:33 +0100

    Añadida tabla de productos

--- Web/Pages/Product/ProductSearch.aspx.cs
+++ Web/Pages/Product/ProductSearch.aspx.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Es.Udc.DotNet.ModelUtil.IoC;
+using Es.Udc.DotNet.PracticaMaD.Model.ProductService;
+using Es.Udc.DotNet.PracticaMaD.Web.Properties;
+using System;
 using System.Collections;
+using System.Configuration;
 using System.Data;
+using System.Web;
 
 namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.Product
 {
@@ -8,6 +13,73 @@ namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.Product
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            int startIndex, count;
+
+            lnkPrevious.Visible = false;
+            lnkNext.Visible = false;
+
+            /* Get Start Index */
+            try
+            {
+                startIndex = int.Parse(Request.Params.Get("startIndex"));
+            }
+            catch (ArgumentNullException)
+            {
+                startIndex = 0;
+            }
+
+            /* Get Count */
+            try
+            {
+                count = int.Parse(Request.Params.Get("count"));
+            }
+            catch (ArgumentNullException)
+            {
+                count = int.Parse(ConfigurationManager.AppSettings["defaultCount"]);
+            }
+
+            /* Get the Service */
+            IIoCManager iocManager = (IIoCManager)HttpContext.Current.Application["managerIoC"];
+            IProductService accountService = iocManager.Resolve<IProductService>();
+
+            /* Get Accounts Info */
+            ProductBlock productBlock =
+                accountService.FindAllProducts(startIndex, count);
+
+            if (productBlock.Products.Count == 0)
+            {
+                lblNoProducts.Visible = true;
+                return;
+            }
+
+            gvProducts.DataSource = productBlock.Products;
+            gvProducts.DataBind();
+
+            /* "Previous" link */
+            if ((startIndex - count) >= 0)
+            {
+                string url =
+                    Settings.Default.PracticaMaD_applicationURL +
+                    "/Pages/ProductSearch.aspx" + "?startIndex=" + (startIndex - count) +
+                    "&count=" + count;
+
+                lnkPrevious.NavigateUrl = Response.ApplyAppPathModifier(url);
+                lnkPrevious.Visible = true;
+            }
+
+            /* "Next" link */
+            if (productBlock.ExistMoreProducts)
+            {
+                string url =
+                    Settings.Default.PracticaMaD_applicationURL +
+                    "/Pages/ShowAccountsByUserID.aspx" + "?startIndex=" + (startIndex + count) +
+                    "&count=" + count;
+
+                lnkNext.NavigateUrl =
+                    Response.ApplyAppPathModifier(url);
+                lnkNext.Visible = true;
+            }
+
             // Load data for the DropDownList control only once, when the 
             // page is first loaded.
             if (!IsPostBack)
--- Web/Pages/Product/ProductSearch.aspx.designer.cs
+++ Web/Pages/Product/ProductSearch.aspx.designer.cs
@@ -22,15 +22,6 @@ namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.Product {
         protected global::System.Web.UI.WebControls.Localize lclMenuExplanation;
         
         /// <summary>
-        /// Control lnkRegister.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink lnkRegister;
-        
-        /// <summary>
         /// Control ProductSearchForm.
         /// </summary>
         /// <remarks>
@@ -67,12 +58,48 @@ namespace Es.Udc.DotNet.PracticaMaD.Web.Pages.Product {
         protected global::System.Web.UI.WebControls.DropDownList CategoryDropDownList;
         
         /// <summary>
-        /// Control btnLogin.
+        /// Control btnSearch.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnLogin;
+        protected global::System.Web.UI.WebControls.Button btnSearch;
+        
+        /// <summary>
+        /// Control lblNoProducts.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNoProducts;
+        
+        /// <summary>
+        /// Control gvProducts.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvProducts;
+        
+        /// <summary>
+        /// Control lnkPrevious.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink lnkPrevious;
+        
+        /// <summary>
+        /// Control lnkNext.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink lnkNext;
     }
 }
