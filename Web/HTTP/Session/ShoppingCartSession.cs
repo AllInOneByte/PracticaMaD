4874af1013c4ed5c364582543a87e69003804ba5
Alejandro Pereira Carballo <alejandro.pereira.carballo@udc.es>
2021-01-10 18:30:02 +0100

    Vinculado botón de añadir a carrito con función de code behind

--- Web/HTTP/Session/ShoppingCartSession.cs
+++ Web/HTTP/Session/ShoppingCartSession.cs
@@ -1,25 +1,12 @@
-﻿using System;
-using Es.Udc.DotNet.PracticaMaD.Model.ShoppingService;
+﻿using Es.Udc.DotNet.PracticaMaD.Model.ShoppingService;
 using System.Collections.Generic;
 
 namespace Es.Udc.DotNet.PracticaMaD.Web.HTTP.Session
 {
     public class ShoppingCartSession
     {
+        public List<ShoppingCart> ShoppingCart { get; set; }
 
-        private List<ShoppingCart> shoppingCart;
-        private string address;
-
-        public List<ShoppingCart> ShoppingCart
-        {
-            get { return shoppingCart; }
-            set { shoppingCart = value; }
-        }
-
-        public string Address
-        {
-            get { return address; }
-            set { address = value; }
-        }
+        public string Address { get; set; }
     }
 }
