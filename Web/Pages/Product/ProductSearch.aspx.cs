ca0833069ae359c6922371d19a31e85debb370de
Alejandro Pereira Carballo <alejandro.pereira.carballo@udc.es>
2021-01-10 17:20:21 +0100

    Añadida localización para ProductSearch

--- Web/Pages/Product/ProductSearch.aspx.cs
+++ Web/Pages/Product/ProductSearch.aspx.cs
@@ -4,7 +4,6 @@ using Es.Udc.DotNet.PracticaMaD.Model.ProductService;
 using Es.Udc.DotNet.PracticaMaD.Web.Properties;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Data;
 using System.Web;
 
